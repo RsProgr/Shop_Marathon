7641d0ba5622c196db3f6197b9316b8b3e13618a
Your Name <you@example.com>
2024-06-08 13:19:29 +0300

    upd

--- src/shop/dao/Shop.java
+++ src/shop/dao/Shop.java
@@ -1,4 +1,23 @@
 package shop.dao;
 
+import shop.model.Computer;
+
 public interface Shop {
+    boolean addComputer(Computer computer);
+
+    Computer removeComputer(long serialNumber);
+
+    Computer findComputer(long serialNumber);
+
+    int quantity();
+
+    double price();
+
+    double discount();
+
+    void printComputer();
+
+    Computer updateComputer(long serialNumber, String brand);
+
+
 }
