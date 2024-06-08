20f2e38a61990aa799d63d6db35d7652fccabf08
Your Name <you@example.com>
2024-06-08 13:45:13 +0300

    added test

---
+++ src/shop/tests/ShopImplTest.java
@@ -0,0 +1,47 @@
+package shop.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import shop.model.Computer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopImplTest {
+    Computer[] computer;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addComputer() {
+    }
+
+    @Test
+    void removeComputer() {
+    }
+
+    @Test
+    void findComputer() {
+    }
+
+    @Test
+    void quantity() {
+    }
+
+    @Test
+    void price() {
+    }
+
+    @Test
+    void discount() {
+    }
+
+    @Test
+    void printComputer() {
+    }
+
+    @Test
+    void updateComputer() {
+    }
+}
